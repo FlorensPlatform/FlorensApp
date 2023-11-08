e0065a09c5ffdba69cc23dd3c590d0ffd1962178
jjyanangomez <juanyasa_04@hotmail.com>
2023-11-08 04:20:01 -0500

    Corregir Navegación

--- Navigation.jsx
+++ Navigation.jsx
@@ -1,6 +1,7 @@
 import react, { useContext } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { navigationRef } from './RootNavigation';
 import { AuthContext } from "./context/AuthContext";
 
@@ -15,14 +16,14 @@ import DesNecesidadesScreen from "./src/views/desNecesidades";
 import DesDominioScreen from "./src/views/desDominios";
 import DesPatronesScreen from "./src/views/desPatrones";
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 const Navigation = () => {
 const {isLoading, login, userInfo} = useContext(AuthContext);
 
   return (
     <NavigationContainer ref={navigationRef}>      
       <Stack.Navigator 
-        initialRouteName="DesPatronesScreen" 
+        initialRouteName="Login" 
         screenOptions={{
           headerMode: 'screen',
           headerTintColor: 'White',
@@ -32,16 +33,26 @@ const {isLoading, login, userInfo} = useContext(AuthContext);
         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
         <Stack.Screen name="Registro" component={RegisScreen} options={{headerShown:false}}/>
         <Stack.Screen name="Contenido" component={ContenidoScreen} options={{headerShown:false}}/>
-        <Stack.Screen name="Necesidades" component={NecesidadesScreen} />
-        <Stack.Screen name="Patrones" component={PatronesScreen} />
-        <Stack.Screen name="Dominios" component={DominiosScreen} />
-        <Stack.Screen name="DesNecesidades" component={DesNecesidadesScreen} />
-        <Stack.Screen name="DesDominios" component={DesDominioScreen} />
-        <Stack.Screen name="DesPatrones" component={DesPatronesScreen} />
+        <Stack.Screen name="Necesidades" component={TabNavigator} />
+        <Stack.Screen name="Patrones" component={TabNavigator} />
+        <Stack.Screen name="Dominios" component={TabNavigator} />
+        <Stack.Screen name="DesNecesidades" component={DesNecesidadesScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="DesDominios" component={DesDominioScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="DesPatrones" component={DesPatronesScreen} options={{headerShown:false}}/>
         
       </Stack.Navigator>
+      
     </NavigationContainer>
+    
   );
 }
-
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Stack.Screen name="Necesidades" component={NecesidadesScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Patrones" component={PatronesScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Dominios" component={DominiosScreen} options={{headerShown:false}}/>
+    </Tab.Navigator>
+  );
+}
 export default Navigation;
