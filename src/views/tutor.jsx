bdeec7ea3ace5cb382530db34f089115f31e9c5e
jjyanangomez <juanyasa_04@hotmail.com>
2024-01-10 02:06:10 -0500

    Cambios del chat presentar mensajes de usuario

--- src/views/tutor.jsx
+++ src/views/tutor.jsx
@@ -1,47 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
-import { BASE_URL } from '../config';
+import { Alert, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import axios from 'axios';
+import { BASE_URL } from '../config';
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    // Cargar mensajes iniciales o realizar otras acciones al montar el componente
     loadInitialMessages();
   }, []);
 
   const loadInitialMessages = () => {
-    // Puedes cargar mensajes iniciales desde algún lugar, si es necesario
-    // Por ejemplo, cargar mensajes de una conversación previa
-    setMessages([...messages]);
+    setMessages([
+      // Ejemplo de mensaje del bot
+      {
+        _id: 1,
+        text: 'Hola, ¿cómo puedo ayudarte?',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Asistente',
+          avatar: 'https://lh3.googleusercontent.com/ogw/ANLem4anf8RVn1mOTxOoo3fB8lyB2aU2vVSUV2tpBWRZNDg=s32-c-mo',
+        },
+      },
+    ]);
   };
 
   const onSend = async (newMessages = []) => {
-    // Enviar el nuevo mensaje al servidor
     const userMessage = newMessages[0];
     const response = await sendUserMessage(userMessage.text);
 
     // Obtener la respuesta del servidor y agregarla a los mensajes
     const botMessage = {
       _id: Math.random().toString(),
-      text: response.data.Respuesta, // Ajusta la propiedad según la estructura de la respuesta
+      text: response.data.Respuesta,
       createdAt: new Date(),
       user: {
         _id: 2,
         name: 'Asistente',
+        avatar: 'https://lh3.googleusercontent.com/ogw/ANLem4anf8RVn1mOTxOoo3fB8lyB2aU2vVSUV2tpBWRZNDg=s32-c-mo',
       },
     };
 
-    setMessages((prevMessages) => GiftedChat.append(prevMessages, [botMessage]));
+    // Actualizar el estado de los mensajes
+    setMessages((prevMessages) => GiftedChat.append(prevMessages,[botMessage,userMessage]));
+    
   };
 
   const sendUserMessage = async (text) => {
     const data = { text };
 
     try {
-      const response = await axios.post(`${BASE_URL}/Chat/Chatbot`, {Mensaje: data});
+      // Realizar la solicitud POST al servidor con el texto del usuario
+      const response = await axios.post(`${BASE_URL}/Chat/Chatbot`, { Mensaje: data });
+
+      // Imprimir la respuesta del servidor (útil para depurar)
+      console.log('Respuesta del servidor:', response.data);
+      console.log(messages);
       return response;
     } catch (error) {
       console.error('Error al enviar el mensaje:', error);
